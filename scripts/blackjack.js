3c0df6e5a9d99d3159e9dcce2d9b348f29e2f9ea
Dearon <dearon@gmail.com>
2013-09-03 23:01:46 +0200

    Added more code to make the game work

--- scripts/blackjack.js
+++ scripts/blackjack.js
@@ -1,32 +1,87 @@
+var gameState = {};
+gameState['cards'] = {};
+gameState['total'] = {};
+gameState['cards']['player'] = [];
+gameState['total']['player'] = 0;
+gameState['cards']['dealer'] = [];
+gameState['total']['dealer'] = 0;
+
 randomCard = function() {
 	var key = Math.floor(Math.random() * deck.length);
 	return deck[key];
 }
 
-var state = {};
-state['player'] = {};
-state['player']['cards'] = [];
-
 startGame = function() {
-	gui.drawNew();
+	gui.drawDealButtons();
+
+	$("#deal").click(function() {
+		startRound();
+	});
+}
+
+startRound = function() {
+	gui.drawRoundButtons();
+	gui.drawCardArea();
 
 	$("#hit").click(function() {
-		hitCard('player');
+		hit();
 	});
 	$("#stay").click(function() {
 		alert('Stay pressed');
 	});
 
-	hitCard('player');
-	hitCard('player');
-	hitCard('dealer');
+	hitCard('player', true);
+	hitCard('player', true);
+	hitCard('dealer', true);
+	hitCard('dealer', false);
 
+	gui.drawCards();
+	updateTotal();
+	gui.updateStatus();
 }
 
-hitCard = function(who) {
+hit = function() {
+	hitCard('player', true);
+	gui.drawCards();
+	updateTotal();
+	gui.updateStatus();
+}
+
+hitCard = function(who, visible) {
 	card = randomCard();
-	state['player']['cards'].push(card);
-	gui.drawCard(card, who);
+	card.visible = visible;
+	gameState['cards'][who].push(card);
+}
+
+updateTotal = function()
+{
+	gameState['total']['player'] = calculateTotal(gameState['cards']['player']);
+	gameState['total']['dealer'] = calculateTotal(gameState['cards']['dealer']);
+}
+
+calculateTotal = function(cards) {
+	total = 0;
+	aces = 0;
+
+	for (var i = 0; i < cards.length; i++) {
+		card = cards[i];
+
+		if (card.value.length == 1) {
+			total += card.value[0];
+		} else {
+			aces += 1;		
+		}
+	}
+
+	for (var i = 0; i < aces; i++) {
+		if (aces == 1 && total <= 10) {
+			total += 11;
+		} else {
+			total += 1;
+		}
+	}
+
+	return total;
 }
 
 $(document).ready(function() {
--- scripts/gui.js
+++ scripts/gui.js
@@ -1,17 +1,41 @@
 var gui = {
-	drawNew: function() {
+	drawDealButtons: function() {
+		$('.buttons').html('<a href="#" id="deal" class="button">Deal</a>');
+	},
+
+	drawRoundButtons: function() {
 		$('.buttons').html('<a href="#" id="hit" class="button">Hit</a> <a href="#" id="stay" class="button">Stay</a>');
+	},
+
+	drawCardArea: function() {
 		$('.container').append('<div class="eight columns center"><h4>Your cards</h4><div id="playerCards"></div></div>');
 		$('.container').append('<div class="eight columns center"><h4>Dealers cards</h4><div id="dealerCards"></div></div>');
 	},
 
-	drawCard: function(card, person) {
-		if (person == 'player') {
-			$("#playerCards").append(card.name + "<br />");
+	drawCards: function() {
+		$("#playerCards").empty();
+		$("#dealerCards").empty();
+
+		for (var i = 0; i < gameState.cards.player.length; i++) {
+			card = gameState.cards.player[i];
+			if (card.visible) {
+				$("#playerCards").append(card.name + "<br />");
+			}
 		}
 
-		if (person == 'dealer') {
-			$("#dealerCards").append(card.name + "<br />");
+		for (var i = 0; i < gameState.cards.dealer.length; i++) {
+			card = gameState.cards.dealer[i];
+			if (card.visible) {
+				$("#dealerCards").append(card.name + "<br />");
+			}
+		}
+	},
+
+	updateStatus: function() {
+		if (gameState['total']['player'] > 21) {
+			$("#status").html('You have gone bust');
+		} else {
+			$("#status").html('You have ' + gameState['total']['player'] + ' points');
 		}
 	}
 }
